8816d245a9b67cc30cf775c3784648d914c46e50
Kaung Zaw <kzaw@myseneca.ca>
2023-06-25 13:40:41 -0400

    - changed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // For VERCEL deployment
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
 })
